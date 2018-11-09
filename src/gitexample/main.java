012eaf7fa17b9ec61eb9f700b6fd439fda779c23
PC-8801 Mk II SR <PC-8801 Mk II SR@10.37.114.59>
2018-11-09 10:39:16 -0500

    more txt

--- src/gitexample/main.java
+++ src/gitexample/main.java
@@ -3,8 +3,9 @@ package gitexample;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("The YM2608 is the best FM chip");
 
 	}
+	
 
 }
